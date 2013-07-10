d2a8e8045c675a7f716f2f6073d2cace8993f0b1
Nolboo Kim <nolboo.kim@gmail.com>
2013-07-10 10:14:42 +0000

    Added web.js with fs.readFileSync, buf.toString

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf;
+  buf = fs.readFileSync( 'index.html' );
+  response.send( buf.toString() );
 });
 
 var port = process.env.PORT || 5000;
